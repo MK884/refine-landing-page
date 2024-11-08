98583c7018768f4210f62c72f4fde670f6750d6a
MK884 <marchantkhalid7810@gmail.com>
2024-11-08 20:44:01 +0530

    hero section

--- src/sections/hero/index.tsx
+++ src/sections/hero/index.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import style from "./style.module.scss";
 import { FaStar } from "react-icons/fa6";
+import { CiPlay1 } from "react-icons/ci";
+import { MdDone } from "react-icons/md";
 
 function HeroSection() {
+  const [isClicked, setIsClicked] = React.useState<boolean>(false);
+
+
+  const npm_command = 'npm create refine-app@latest'
+
+  const handleClickNpm = () => {
+    setIsClicked(true);
+    navigator.clipboard.writeText(npm_command)
+    setTimeout(() => setIsClicked(false), 5000);
+  };
+
   return (
     <section className={style.section}>
       <div className={style.info}>
@@ -15,6 +28,40 @@ function HeroSection() {
             </div>
           </a>
         </button>
+        <div className={style.head}>
+          <h1>Open-source Retool for Enterprise</h1>
+          <p className={style.caption}>
+            Build React-based internal tools, admin panels, dashboards & B2B
+            apps with unmatched flexibility.
+          </p>
+        </div>
+        <div className={style.actions}>
+          <button className={style.doc_btn}>
+            <CiPlay1 size={22} />
+            see docs
+          </button>
+          <button className={style.npm_btn} onClick={handleClickNpm}>
+            <span
+              data-text={npm_command}
+              className={style.npm}
+            >
+              {npm_command}
+              {isClicked && (
+                <>
+                  <span className={style.copy} data-text="copied to clipboard!">
+                    copied to clipboard!
+                  </span>
+                </>
+              )}
+            </span>
+            <div className={style.bg} />
+            {isClicked && (
+              <div className={style.icon}>
+                <MdDone size={14} />
+              </div>
+            )}
+          </button>
+        </div>
       </div>
     </section>
   );
